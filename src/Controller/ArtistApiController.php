c01c8bec0f5e1cbcbc365667421c55792e1b9bc4
Enzobu <enzo.palermo@epsi.fr>
2024-11-10 16:55:18 +0100

    add artistcomments for swagger

--- src/Controller/ArtistApiController.php
+++ src/Controller/ArtistApiController.php
@@ -15,6 +15,27 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class ArtistApiController extends AbstractController
 {
+    /**
+     * @OA\Tag(name="Artist", description="API pour gérer les artistes")
+     */
+
+    /**
+     * @OA\Get(
+     *     path="/api/artist/get-artists",
+     *     tags={"Artist"},
+     *     summary="Get all artists",
+     *     @OA\Response(response="200", description="List of all artists",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(
+     *                 @OA\Property(property="id", type="integer"),
+     *                 @OA\Property(property="name", type="string"),
+     *                 @OA\Property(property="illustration", type="string")
+     *             )
+     *         )
+     *     )
+     * )
+     */
     #[Route('/api/artist/get-artists', name: 'app_gat_artist', methods: ["GET"])]
     public function getArtist(
         ArtistRepository         $artistRepository,
@@ -25,6 +46,28 @@ class ArtistApiController extends AbstractController
         $response = $serializer->serialize($artist, "json", ["groups" => "Artist"]);
         return $this->json(json_decode($response), Response::HTTP_OK);
     }
+
+    /**
+     * @OA\Post(
+     *     path="/api/artist/add-artist",
+     *     tags={"Artist"},
+     *     summary="Add a new artist",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(property="name", type="string", description="Name of the artist"),
+     *             @OA\Property(property="illustration", type="string", description="Illustration URL of the artist")
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Artist created successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="id", type="integer"),
+     *             @OA\Property(property="name", type="string"),
+     *             @OA\Property(property="illustration", type="string")
+     *         )
+     *     )
+     * )
+     */
     #[Route('/api/artist/add-artist', name: 'app_add_artist', methods: ["POST"])]
     public function addArtist(
         ArtistRepository         $artistRepository,
@@ -53,6 +96,21 @@ class ArtistApiController extends AbstractController
             "artist"=> $response,
         ], Response::HTTP_OK);
     }
+
+    /**
+     * @OA\Delete(
+     *     path="/api/artist/remove-artist",
+     *     tags={"Artist"},
+     *     summary="Delete an artist",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="query",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(response="200", description="Artist deleted successfully")
+     * )
+     */
     #[Route('/api/artist/remove-artist', name: 'app_remove_artist', methods: ["DELETE"])]
     public function removeArtist(
         ArtistRepository         $artistRepository,
@@ -75,6 +133,34 @@ class ArtistApiController extends AbstractController
             "message" => "Artist supprimé avec succès",
         ], Response::HTTP_OK);
     }
+
+    /**
+     * @OA\Put(
+     *     path="/api/artist/update-artist",
+     *     tags={"Artist"},
+     *     summary="Update an artist",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="query",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(property="name", type="string", description="Updated name of the artist"),
+     *             @OA\Property(property="illustration", type="string", description="Updated illustration URL of the artist")
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Artist updated successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="id", type="integer"),
+     *             @OA\Property(property="name", type="string"),
+     *             @OA\Property(property="illustration", type="string")
+     *         )
+     *     )
+     * )
+     */
     #[Route('/api/artist/update-artist', name: 'app_update_artist', methods: ["PUT"])]
     public function updateArtist(
         ArtistRepository         $artistRepository,
